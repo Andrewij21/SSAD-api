1ba48e92f7915d6e95afa5317bbfb9d9b044534c
Andrewij21 <andrewijaya988@gmail.com>
2023-10-24 20:27:00 +0700

    refactor: change rror msg and props name

--- src/middlewares/verifyJwt.js
+++ src/middlewares/verifyJwt.js
@@ -2,15 +2,18 @@ const jwt = require("jsonwebtoken");
 
 const verifyJWT = (req, res, next) => {
   const token = req.headers["authorization"]?.split(" ")[1];
-  if (!token) return res.sendStatus(401);
+  if (!token) return res.status(401).json({ error: "Null token" });
 
   try {
-    const { roles, email } = jwt.verify(token, process.env.SECRET_ACCESS_TOKEN);
-    req.email = email;
+    const { roles, username } = jwt.verify(
+      token,
+      process.env.SECRET_ACCESS_TOKEN
+    );
+    req.username = username;
     req.roles = roles;
     next();
   } catch (error) {
-    res.status(403).json({ error });
+    res.status(403).json({ error: "Expired token" });
   }
 };
 
