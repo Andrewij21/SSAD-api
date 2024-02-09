e4fd71e622fbb93f3f7b4fe63d7cd9e06e3925be
andrewijaya <andrewijaya988@gmail.com>
2024-02-09 13:26:46 +0700

    update: add new access origin 4000

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://127.0.0.1:5174", "http://localhost:3000"];
+const allowedOrigins = [
+  "http://127.0.0.1:5174",
+  "http://localhost:3000",
+  "http://localhost:4000",
+];
 
 module.exports = allowedOrigins;
